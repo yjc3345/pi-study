ff0bc004c1a25308f3698a7cb83c5398f4ccf22f
yjc3345 <yjc3345@naver.com>
2023-02-04 15:47:31 +0900

    4thJan_dropdown

--- mypage/index.js
+++ mypage/index.js
@@ -4,19 +4,23 @@ const closeBtn = document.querySelector('.close');
 const modal = document.querySelector('.modal');
 const modalBtn = document.querySelector('#modalBtn');
 
-function dropdownBtnClick() {
-	if (dropdown.style.display === 'block') {
-		dropdown.style.display = 'none';
-	} else {
-		dropdown.style.display = 'block';
-	}
-}
+dropdownBtn.addEventListener('click', function () {
+	dropdownBtn.classList.toggle('active');
+});
+
+// function dropdownBtnClick() {
+// 	if (dropdown.style.display === 'block') {
+// 		dropdown.style.display = 'none';
+// 	} else {
+// 		dropdown.style.display = 'block';
+// 	}
+// }
 function closeOnClick() {
 	modal.style.display = 'none';
 }
 function modalOnclick() {
 	modal.style.display = 'block';
 }
-dropdownBtn.onclick = dropdownBtnClick;
+// dropdownBtn.onclick = dropdownBtnClick;
 closeBtn.onclick = closeOnClick;
 modalBtn.onclick = modalOnclick;
